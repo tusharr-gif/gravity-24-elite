740fcf66904b70570db8809e529210158d930853
tushar sonar <tusharpotdar16@gmail.com>
2026-05-27 14:32:27 +0530

    Revert invalid server preset option in vite.config.ts to fix compilation

--- vite.config.ts
+++ vite.config.ts
@@ -6,11 +6,7 @@ import tsConfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [
-    tanstackStart({
-      server: {
-        preset: "vercel"
-      }
-    }),
+    tanstackStart(),
     react(),
     tailwindcss(),
     tsConfigPaths({ projects: ["./tsconfig.json"] }),
